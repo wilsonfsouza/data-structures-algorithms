352c130a713c4878cb9388cc83b44740c1704dc8
Wilson Franca <wilson.franca.92@gmail.com>
2023-12-02 06:30:59 -0800

    feat(insertion-sort): fixed algo to return sorted array using insertion-sort method

--- src/algorithms/03_sort-algorithms/insertion-sort/me.js
+++ src/algorithms/03_sort-algorithms/insertion-sort/me.js
@@ -1,48 +1,46 @@
+// assuming the sorting order to ascending
+// assume first element of the array is already sorted (-6)
+// 2 "subarrays" - 1 is sorted and 1 is unsorted
+
 /**
  * 
  * @param {Array<Number>} array 
  */
 function insertionSort(array) {
-  // assuming the sorting order to ascending
-  // assume first element of the array is already sorted (-6)
-  // 2 "subarrays" - 1 is sorted and 1 is unsorted
-  let isSorted = false
-  
   // variable to store the number of the unsorted array to be placed within the sorted ones
   let indexOfNumberToBeInserted = 1
-  let numberToBeInserted = array[indexOfNumberToBeInserted]
-
-  // variable to store the marker of each sorted element that is being checked
-  let sortedElementIndex = 0
-  let sortedElement = array[sortedElementIndex]
 
   let lastElementOfArray = array.length - 1
 
+  let isSorted = false
+
   while(isSorted !== true) {
+    if (indexOfNumberToBeInserted > lastElementOfArray) {
+      isSorted = true
+      return
+    }
+
+    let numberToBeInserted = array[indexOfNumberToBeInserted]
+
     // check the number to be inserted against the sorted element
-    for (let index = indexOfNumberToBeInserted - 1; (index >= 0) && (index < indexOfNumberToBeInserted); index--) {
+    for (let index = indexOfNumberToBeInserted - 1; (index >= 0) && (index <= indexOfNumberToBeInserted); index--) {
+      let sortedElement = array[index]
+
       // If sorted element > number to be inserted
       // No -> keep number to be inserted to the right
       if (sortedElement < numberToBeInserted) {
         break
       }
       
-      // Yes -> shift sorted element to the right
-      array[indexOfNumberToBeInserted] = sortedElement
-      
-      if (sortedElementIndex === 0) {
-        array[sortedElementIndex] = numberToBeInserted
-        break
+      if (sortedElement > numberToBeInserted) {
+        // Yes -> shift sorted element to the right
+        let elementToTheRightOfSortedElement = index + 1
+        array[elementToTheRightOfSortedElement] = sortedElement
+        array[index] = numberToBeInserted
       }
-
-      sortedElementIndex--
     }
 
     indexOfNumberToBeInserted++
-
-    if (indexOfNumberToBeInserted > lastElementOfArray) {
-      isSorted = true
-    }
   }
 }
 
